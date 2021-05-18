1bb8243ba35950f9459ea88704160dec40effe2e
Matthew Aquino <matthewaquino96@gmail.com>
2021-05-18 08:57:56 -0400

    Time: 76 ms (79.92%), Space: 39.2 MB (16.00%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -5,17 +5,29 @@
  */
 var twoSum = function(nums, target) {
     
-    let num2;
-    let half;
+//     let num2;
+//     let half;
     
-    for (let i = 0; i <= nums.length; i++) {
-        if (target === 0 && nums.includes(0)) { 
-            return [nums.indexOf(0), nums.lastIndexOf(0)]
-        } 
-        num2 = target - nums[i];
-        if ( target / nums[i] === 2 && nums.lastIndexOf(num2) !== i) { return [i,                   nums.lastIndexOf(num2)] }
-        if (nums.includes(num2) && nums.lastIndexOf(num2) !== i) {
-            return [i, nums.indexOf(num2)]}
+//     for (let i = 0; i <= nums.length; i++) {
+//         if (target === 0 && nums.includes(0)) { 
+//             return [nums.indexOf(0), nums.lastIndexOf(0)]
+//         } 
+//         num2 = target - nums[i];
+//         if ( target / nums[i] === 2 && nums.lastIndexOf(num2) !== i) { 
+//             return [i, nums.lastIndexOf(num2)] }
+//         if (nums.includes(num2) && nums.lastIndexOf(num2) !== i) {
+//             return [i, nums.indexOf(num2)]}
+//     }
+    
+    const solutionObj = {}
+    
+    for ( let i = 0; i < nums.length; i++ ) {
+        let complement = target - nums[i];
+        if (solutionObj.hasOwnProperty(complement)) {
+            return [ solutionObj[complement], i ]
+        }
+        solutionObj[nums[i]] = i 
     }
+    
     
 };
