17503816a9fd301c9d952fe6f54182166186a16c
Matthew Aquino <matthewaquino96@gmail.com>
2021-06-05 05:05:19 -0400

    Time: 140 ms (5.17%), Space: 40 MB (88.65%) - LeetHub

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+
+    let list = new ListNode()
+    let head = list
+    
+    while (l1 !== null && l2 !== null) {
+	
+		// Select the smallest value from either linked list,
+		// then increment that list forward.
+        if (l1.val < l2.val) {
+            list.next = new ListNode(l1.val)
+            l1 = l1.next
+        } else {
+            list.next = new ListNode(l2.val)
+            l2 = l2.next
+        }
+        
+        list = list.next
+    }
+    
+	// It's possible that one linked list is shorter than the other so we just
+	// add on the remainder of the last linked list. It's already sorted :)
+    
+    // if (l1 !== null)
+    //     list.next = l1
+    // if (l2 !== null)
+    //     list.next = l2
+    
+    list.next = l1 || l2
+    
+	// return .next because this first element in the linkedlist is empty
+    return head.next
+    
+    
+};
+
