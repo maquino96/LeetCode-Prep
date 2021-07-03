97cac0b2898523defd404b70d95efb402bc8a135
Matthew Aquino <matthewaquino96@gmail.com>
2021-07-03 15:28:58 -0400

    Time: 84 ms (90.94%), Space: 48.2 MB (24.39%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let maxArea = 0;
+  let i = 0;
+  let j = height.length - 1;
+  while (i < j) {
+    maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+    if (height[i] < height[j]) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return maxArea;
+
+    
+};
+
+/*
+
+input array of ints
+output max area
+
+examples: multiple pointer // sliding window
+
+*/
