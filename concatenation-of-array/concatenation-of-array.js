7bd59ddd2985e37361ff665d97a0e6e021afbd52
Matthew Aquino <matthewaquino96@gmail.com>
2021-07-22 23:26:00 -0400

    Time: 96 ms (45.35%), Space: 43.1 MB (5.48%) - LeetHub

---
+++ concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let ans = []
+    
+    for ( let i = 0; i< nums.length*2; i++){
+        ans.push(nums[i%nums.length])
+    }
+    
+    return ans
+};
