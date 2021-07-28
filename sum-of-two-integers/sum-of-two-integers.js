4000320e17525b7bdd36cf6bad396a0aac2b0fb9
Matthew Aquino <matthewaquino96@gmail.com>
2021-07-27 20:08:24 -0400

    Time: 76 ms (45.96%), Space: 38.7 MB (36.87%) - LeetHub

---
+++ sum-of-two-integers/sum-of-two-integers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    let count = 0
+    
+    for ( let i = 0; a < 0? i > a: i < a; a < 0 ? i--: i++){
+        a < 0? count-- : count++
+    }
+    
+    
+    for ( let j = 0; b < 0? j > b : j < b; b < 0 ? j-- : j++){
+        b < 0? count-- : count++
+        // console.log(count)
+    }
+    
+    return count
+};
