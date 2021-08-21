c5670cb0c038c439faeec782a5c6c923e657542f
Matthew Aquino <matthewaquino96@gmail.com>
2021-08-21 16:42:30 -0400

    Time: 100 ms (48.37%), Space: 48.7 MB (86.40%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,62 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // brute force, exceeds time limit
-    // let maxprofit = 0;     
-    // for ( let i = 0; i < prices.length-1; i++) {
-    //     for ( let j = i+1; j < prices.length; j++) {
-    //         profit = prices[j] - prices[i];
-    //         if ( profit > maxprofit ) { maxprofit = profit }
-    //     }
-    // }
-    // return maxprofit
+    let maxProfit = 0
+    let minPrice = Infinity
     
+    for (let i = 0; i < prices.length; i++){
+        if (prices[i] < minPrice) minPrice = prices[i]
+        if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice
+    }
     
-/*
-approach: 
-
-let p1 = 0
-let p2 = 1
-
-
-*/
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     In formal terms, we need to find \max(\text{prices[j]} - \text{prices[i]})max(prices[j]−prices[i]), for every ii and jj such that j > ij>i.
-    
-      
-    // one pass
-    
-    let minprice = Math.max(...prices)
-    let maxprofit = 0
-    
-    for ( let i = 0; i < prices.length; i++) {
-        if ( prices[i] < minprice) {
-            minprice = prices[i]
-        } else if ( prices[i] - minprice > maxprofit ) {
-            maxprofit = prices[i] - minprice;
-        }
-     }
-     return maxprofit
+    return maxProfit
 };
