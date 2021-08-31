049a6070a5370868b4d7e005938c0b4f861e1f1f
Matthew Aquino <matthewaquino96@gmail.com>
2021-08-30 22:43:29 -0400

    Time: 144 ms (5.80%), Space: 40.1 MB (94.56%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.js
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -12,66 +12,30 @@
  */
 var mergeTwoLists = function(l1, l2) {
     
-    let list = new ListNode();
-    let head = list
+    if ( !l1 && !l2) return l1
+    if (!l1) return l2
+    if (!l2) return l1
     
-    while (l1 !== null && l2!==null){
-        
-        if( l1.val < l2.val ) {
-            list.next = new ListNode(l1.val)
+    let dummy = new ListNode(0)
+    let result = dummy; 
+    while (l1 && l2){
+        if (l1.val <= l2.val ) {
+            dummy.next = l1
             l1 = l1.next
         } else {
-            list.next = new ListNode(l2.val)
-            l2 = l2.next
+            dummy.next = l2
+            l2=l2.next
         }
-        list = list.next
+        dummy = dummy.next
     }
-    
-    if ( l1 === null ) list.next = l2;
-    if ( l2 === null ) list.next = l1
-    
-    return head.next
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//     let list = new ListNode()
-//     let head = list
-    
-//     while (l1 !== null && l2 !== null) {
-	
-// 		// Select the smallest value from either linked list,
-// 		// then increment that list forward.
-//         if (l1.val < l2.val) {
-//             list.next = new ListNode(l1.val)
-//             l1 = l1.next
-//         } else {
-//             list.next = new ListNode(l2.val)
-//             l2 = l2.next
-//         }
-        
-//         list = list.next
-//     }
-    
-// 	// It's possible that one linked list is shorter than the other so we just
-// 	// add on the remainder of the last linked list. It's already sorted :)
-//     if (l1 !== null)
-//         list.next = l1
-//     if (l2 !== null)
-//         list.next = l2
-    
-// 	// return .next because this first element in the linkedlist is empty
-//     return head.next
-    
+
+    if (l1) {
+        dummy.next = l1
+        dummy = dummy.next
+    } 
+    if (l2) { 
+        dummy.next = l2
+        dummy = dummy.next    
+    }
+    return result.next
 };
